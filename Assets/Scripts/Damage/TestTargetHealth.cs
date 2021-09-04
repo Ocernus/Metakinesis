ac833af7e3b8ecd1e3cff3d4b65cb9a1038dff16
Ocernus <56883919+Ocernus@users.noreply.github.com>
2021-09-04 10:05:52 -0700

    Adding sound to test target, cleanup prototyping

---
+++ Assets/Scripts/Damage/TestTargetHealth.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTargetHealth : Damageable
+{
+    public GameObject damageSound;
+
+    public override void TakeDamage(int amount)
+    {
+        Instantiate(damageSound, transform.position, transform.rotation);
+    }
+}
