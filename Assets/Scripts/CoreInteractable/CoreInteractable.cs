b28a7ace378e602b8feba9c85153758ec7f6b1d6
Ocernus <56883919+Ocernus@users.noreply.github.com>
2021-10-10 10:19:21 -0700

    Beginning Of Interactable Rework

---
+++ Assets/Scripts/CoreInteractable/CoreInteractable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreInteractable : MonoBehaviour
+{
+    public virtual void CoreFunction()
+    {
+        // Do thing
+    }
+}
