f4eec705b9b69b84d6921cc0eb6cecae6f250f01
Ocernus <56883919+Ocernus@users.noreply.github.com>
2021-10-25 12:43:09 -0700

    Working on ship construction

---
+++ Assets/Scripts/WorldFunctions/PhylacteryLamp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhylacteryLamp : MonoBehaviour
+{
+    public GameObject graphics;
+    public Vector3 rotation;
+
+    private void Update()
+    {
+        graphics.transform.Rotate(rotation);
+    }
+}
