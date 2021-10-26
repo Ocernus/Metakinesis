231432c5e7d37b93ce1159b1a0239e4bd7a78ece
Ocernus <56883919+Ocernus@users.noreply.github.com>
2021-10-26 12:42:39 -0700

    Working on archive details

---
+++ Assets/Scripts/WorldFunctions/PhylacteryTorus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhylacteryTorus : MonoBehaviour
+{
+    public Vector3 rotation;
+
+    private void Update()
+    {
+        transform.Rotate(rotation);
+    }
+}
