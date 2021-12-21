8d7211826df828b8203256cdcb1440f4d6e41b83
Ocernus <56883919+Ocernus@users.noreply.github.com>
2021-12-21 11:52:41 -0700

    Aimer working

---
+++ Assets/Scripts/Dragon/ParticleAimer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleAimer : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.LookAt(Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 100000f)));
+    }
+}
