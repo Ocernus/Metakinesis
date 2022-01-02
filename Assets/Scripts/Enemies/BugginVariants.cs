8533b8e502fa96528bde1f64b33d076b9234e25a
Ocernus <56883919+Ocernus@users.noreply.github.com>
2022-01-01 18:55:57 -0700

    progress on buggins

---
+++ Assets/Scripts/Enemies/BugginVariants.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugginVariants : MonoBehaviour
+{
+    public GameObject maskMandibles;
+    public GameObject maskArmored;
+    public GameObject maskHooded;
+
+    public GameObject armorShoulder;
+    public GameObject armorForearm;
+    public GameObject armorHips;
+    public GameObject armorLegs;
+
+    public GameObject sleeves;
+    public GameObject gutArmor;
+
+    public GameObject shieldFrame;
+    public GameObject shieldPanelsA;
+    public GameObject shieldPanelsB;
+
+    public GameObject spearShaft;
+    public GameObject spearHead;
+
+    public GameObject crusherHandle;
+    public GameObject crusherBlade;
+
+    public GameObject wings;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
